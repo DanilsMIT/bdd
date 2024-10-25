9f52f220a17bd748198b006f6f2414808b7d6095
DanilsMIT <danilsmitdm@gmail.com>
2024-10-24 22:02:26 -0500

    repositorio repasado

--- M2FINAL SQL P1/Tabla productos.sql
+++ M2FINAL SQL P1/Tabla productos.sql
@@ -35,9 +35,9 @@ insert into categorias(idC, nombre) values
 insert into productos(idP,nombre,precio_venta,precio_compra,ID_Categoria) values
 ('P0001','Coca cola 300 ml', 0.7, 0.55,'C001'),
 ('P0002','Coca cola 1000 ml', 1, 0.8,'C001'),
-('P0003','Doritos 50g', 0.5, 0.42,'C001'),
-('P0004','Manicho', 0.25, 0.21,'C001'),
-('P0005','Tango', 0.5, 0.42,'C001');
+('P0003','Doritos 50g', 0.5, 0.42,'C002'),
+('P0004','Manicho', 0.25, 0.21,'C003'),
+('P0005','Tango', 0.5, 0.42,'C003');
 
 insert into historial_movimientos(id_producto,cantidad,fecha_del_movimiento) values
 ('P0001',10,'28/10/2020 9:45'),
